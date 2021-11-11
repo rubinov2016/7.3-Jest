cedf5520f90a1179b3160c0f0d366cf2e36448dd
Ivan Rubinov <lrubinov@yandex.ru>
2021-11-11 14:04:01 +0300

    First init

---
+++ test/unit/sortByName.test.js
@@ -0,0 +1,20 @@
+const sorting = require("../../app");
+
+describe("Books names test suit", () => {
+  it("Books names should be sorted in ascending order", () => {
+    expect(
+      sorting.sortByName([
+        "Гарри Поттер",
+        "Властелин Колец",
+        "Властелин Колец",
+        "Волшебник изумрудного города",
+      ])
+    ).toEqual([
+      "Властелин Колец",
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+      "Гарри Поттер",
+    ]);
+  });
+});
+
